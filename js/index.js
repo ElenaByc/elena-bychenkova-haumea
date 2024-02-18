52330fa73db601d5cc993a79d2ba918425901f75
Elena Bychenkova <elena.bychenkova@gmail.com>
2024-02-17 16:47:26 -0800

    feat: add skills items from skills array

--- js/index.js
+++ js/index.js
@@ -1,5 +1,3 @@
-console.log("Hi")
-
 // add new element to the footer with current year
 const footer = document.querySelector('.footer');
 const copyrightDiv = document.createElement('div');
@@ -7,3 +5,14 @@ const currentYear = new Date().getFullYear();
 copyrightDiv.classList.add('footer__copyright');
 copyrightDiv.innerHTML = `&copy;&nbsp;Elena&nbsp;Bychenkova&nbsp;${currentYear}`;
 footer.prepend(copyrightDiv);
+
+// add skills from the array to the UL element in the Skills section
+const mySkillsArray = ['Java', 'Python', 'JavaScript', 'HTML', 'CSS', 'Figma', 'Bootstrap', 'SpringBoot', 'Flask', 'PostgreSQL'];
+const skillsSection = document.querySelector('#skills');
+const skillsListElement = skillsSection.querySelector('.skills__list');
+let listItem;
+mySkillsArray.forEach(skill => {
+  listItem = document.createElement('li');
+  listItem.innerText = skill;
+  skillsListElement.appendChild(listItem);
+})
